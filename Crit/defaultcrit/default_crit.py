cb91c3b374e5ab2a4f3730c70338eaa8dcf1e253
Jarod Richardson <jricha94@necluster.ne.utk.edu>
2020-10-23 02:37:57 -0400

    minor changes

--- Crit/defaultcrit/default_crit.py
+++ Crit/defaultcrit/default_crit.py
@@ -33,16 +33,13 @@ for i in range(10):
     enr += 0.25
 
 fit = np.polyfit(enr_list, k_list, 2)
-x = np.arange(0,enr+.25, .1)
 fig1, ax1 = plt.subplots()
 ax1.errorbar(enr_list, k_list, yerr=kerr_list,ls = '', marker=',', label = 'Serpent data')
-ax1.plot(x, fit[0]*x**2 + fit[1]*x + fit[2], label = 'fit')
 ax1.set(xlabel='enrichment (%)', ylabel='k_eff', title='k vs enrichment default fuel')
-ax1.legend()
 fig1.savefig('default.png', transparent=False, dpi=80)
 
 crit_enr_1 = (-1*fit[1]+np.sqrt((fit[1]**2)-(4*fit[0]*(fit[2]-1.))/(2.*fit[0])))
 crit_enr_2 = (-1*fit[1]+np.sqrt((fit[1]**2)-(4*fit[0]*(fit[2]-1.))/(2.*fit[0])))
 crit_enr = [crit_enr_1, crit_enr_2]
 for num in crit_enr:
-     print('The Critcal Enrichment is ', num)
+print('The Critcal Enrichment is ', num)
