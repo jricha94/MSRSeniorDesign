7a6df0b623cff437340f5d3e9282d77b1e8b879d
Jarod Richardson <jarod@pop-os.localdomain>
2020-10-28 15:15:15 -0400

    change salt input

--- Crit/flibecrit/deck.py
+++ Crit/flibecrit/deck.py
@@ -773,7 +773,7 @@ coef {step_cnt} {negsteps} % Negative steps mean days, positive is MWd/kgU
         serpent_inp = 'set title "SF-MSR Concept"'
         serpent_inp += self.surf_and_cells()
         serpent_inp += self.materials()
-        serpent_inp += self.salt.serpent_mat(self.ft, self.fd)
+        serpent_inp += self.salt.serpent_mat()
         serpent_inp += self.get_data_cards()
         return serpent_inp.format(**locals())
 
