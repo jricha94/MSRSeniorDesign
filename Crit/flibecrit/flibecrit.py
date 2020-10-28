3358be8d1a2cb2aaba6a319ed4b6a9a3c59e7e3c
Jarod Richardson <jarod@pop-os.localdomain>
2020-10-28 17:05:17 -0400

    changed enrichment range

--- Crit/flibecrit/flibecrit.py
+++ Crit/flibecrit/flibecrit.py
@@ -10,7 +10,7 @@ import numpy as np
 enr_list = []
 k_list = []
 kerr_list = []
-enr = 0.02
+enr = 0.01
 for i in range(10):
     try:
         os.mkdir('dir{}'.format(i))
@@ -30,15 +30,16 @@ for i in range(10):
     fh.close()
     os.chdir('../')
     shutil.rmtree('dir{}/'.format(i))
-    enr += 0.03
+    enr += 0.01
 enr_list, k_list, kerr_list = np.array(enr_list), np.array(k_list), np.array(kerr_list)
 fit = np.polyfit(np.log(enr_list), k_list, 1)
-x = np.arange(0.01, 0.35, 0.05)
+x = np.arange(0.01, 0.35, 0.001)
 fig1, ax1 = plt.subplots()
-ax1.errorbar(100. * enr_list, k_list, yerr=kerr_list,
-             marker='.', label='Serpent data', color='blue')
+ax1.errorbar(100. * enr_list, k_list, yerr=kerr_list, marker='.',
+             ls='', label='Serpent data', color='blue')
 ax1.plot(100. * x, fit[0] * np.log(x) + fit[1], ls='solid', marker='', label='fit', color='orange')
 ax1.set(xlabel='Enrichment (%)', ylabel='k_eff', title='k vs Enrichment FLiBe')
+ax1.legend()
 fig1.savefig('FLiBe.png', transparent=False, dpi=80)
 
 # crit_enr_1 = (-1 * fit[1] + np.sqrt((fit[1]**2) - (4 * fit[0] * (fit[2] - 1.)) / (2. * fit[0])))
