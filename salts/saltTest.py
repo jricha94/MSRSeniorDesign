7694c4d989a80fb822489c90b05bbfcac7a48224
Jarod Richardson <jarod@pop-os.localdomain>
2020-11-05 20:45:03 -0500

    new dir

--- salts/saltTest.py
+++ salts/saltTest.py
@@ -2,3 +2,14 @@
 
 import osalts
 import nsalts
+
+old = osalts.Salt()
+new = nsalts.Salt()
+
+with open('old.txt', 'x') as o:
+    o.write(old.serpent_mat())
+    o.write(old.serpent_matr())
+    o.write(old.serpent_matp())
+
+with open('new.txt', 'x') as n:
+    n.write(new.serpent_mat())
