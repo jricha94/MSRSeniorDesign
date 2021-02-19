4242c348190ba388dc9abb402172a277ce77f669
Jarod Richardson <jricha94@vols.utk.edu>
2021-02-19 09:10:33 -0500

    added new graph

--- Fuels/TRU_Test/tru_crit.py
+++ Fuels/TRU_Test/tru_crit.py
@@ -13,7 +13,7 @@ kerr_list = []
 enr = 0.003
 UF4_per = 10 #%
 TRU_per = 2  #%
-cleanup = False 
+cleanup = True
 for i in range(10):
     try:
         os.mkdir('dir{}'.format(i))
@@ -36,20 +36,16 @@ for i in range(10):
     os.chdir('../')
     if cleanup == True:
         shutil.rmtree('dir{}/'.format(i))
-    UF4_per -= 0.5
-    TRU_per += 0.5
+    UF4_per -= 1 
+    TRU_per += 1
 TRU_list, k_list, kerr_list = np.array(TRU_list), np.array(k_list), np.array(kerr_list)
 #fit = np.polyfit(np.log(TRU_list), k_list, 1)
 #x = np.arange(0.01, 0.13, 0.001)
 fig1, ax1 = plt.subplots()
-ax1.errorbar(100. * enr_list, k_list, yerr=kerr_list, marker='.',
+ax1.errorbar(TRU_list, k_list, yerr=kerr_list, marker='.',
              ls='', label='Serpent data', color='blue')
 #ax1.plot(100. * x, fit[0] * np.log(x) + fit[1], ls='solid', marker='', label='fit', color='orange')
 ax1.set(xlabel='TRU Fuel Salt Concentration (%)', ylabel='k_eff', title='k vs TRU Concentration')
 ax1.legend()
 fig1.savefig('TRU.png', transparent=False, dpi=80)
 
-# crit_enr_1 = (-1 * fit[1] + np.sqrt((fit[1]**2) - (4 * fit[0] * (fit[2] - 1.)) / (2. * fit[0])))
-# crit_enr_2 = (-1 * fit[1] - np.sqrt((fit[1]**2) - (4 * fit[0] * (fit[2] - 1.)) / (2. * fit[0])))
-# crit_enr = [crit_enr_1, crit_enr_2]
-# print('The Critcal Enrichment is ', crit_enr)
