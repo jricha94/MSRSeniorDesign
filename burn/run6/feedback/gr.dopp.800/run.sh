44ad94de3b3538d0b8805a3ac74a00eaf39f247b
Jarod Richardson <jricha94@vols.utk.edu>
2021-04-20 17:05:12 -0400

    results

---
+++ burn/run6/feedback/gr.dopp.800/run.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+#PBS -V
+#PBS -N ThorCon_like_lat
+#PBS -q xeon
+#PBS -l nodes=1:ppn=64
+hostname
+rm -f done.dat
+cd ${PBS_O_WORKDIR}
+module load mpi
+module load serpent
+sss2 -omp 64 gr.dopp.800 > myout.out
+awk 'BEGIN{ORS="\t"} /ANA_KEFF/ || /CONVERSION/ {print $7" "$8;}' gr.dopp.800_res.m > done.out
+rm gr.dopp.800.out
