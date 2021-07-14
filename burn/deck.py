a1e7dd25d7e93ab1d8b6b6d112d3cb643de35d12
Jarod Richardson <jricha94@vols.utk.edu>
2021-07-14 16:20:13 -0400

    deck update

--- burn/deck.py
+++ burn/deck.py
@@ -91,7 +91,7 @@ class serpDeck(object):
         self.gr_dens:float   = 1.80                          # Graphite density at 950 K [g/cm3]
         self.boron_graphite:float = 2e-06                    # 2ppm boron in graphite
         self.room_temp:float = 293.0                         # room temperature used for thermal expansion
-        self.GLE:bool        = True                          # bool for if to calculate thermal expansion
+        self.thermal_expansion:bool        = True                          # bool for if to calculate thermal expansion
 
         self.k:float    = None                  # k-effective for model
         self.kerr:float = None                  # k-eff error
@@ -172,7 +172,7 @@ class serpDeck(object):
 
 
     def GLE(self, point = None, tempK:float = 293.0) -> list: # Method for Graphite Linear Expansion
-        if self.GLE == True:
+        if self.thermal_expansion == True:
             if type(point) is list: 
                 x0, y0 = point[0], point[1]
                 xf = x0 * (1.0 + GRAPHITE_CTE * (tempK - self.room_temp))
