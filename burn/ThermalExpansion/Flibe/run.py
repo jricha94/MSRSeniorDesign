047533ba55a1ac39cb3590adbf7897da1382d9fd
Jarod Richardson <jricha94@vols.utk.edu>
2021-07-14 23:35:34 -0400

    update run script

--- burn/ThermalExpansion/Flibe/run.py
+++ burn/ThermalExpansion/Flibe/run.py
@@ -1,74 +1,59 @@
 #!/usr/bin/env python3\
 
+import time
 import os
 from burn import burn
 
 # Run with thermal expansion
-tc_run = burn(salt='flibe', rep_salt='flibe')
+te_run = burn(salt='flibe', rep_salt='flibe')
 # Run without thermal expansion
-no_tc_run = burn(salt='flibe', rep_salt='flibe')
+no_te_run = burn(salt='flibe', rep_salt='flibe')
 
 # Set run parameters for each run
-tc_run.queue = 'fill'
-tc_run.ompcores = 8
-tc_run.enr_min = 0.01
-tc_run.enr_max = 0.04
-
-no_tc_run.queue = 'fill'
-no_tc_run.ompcores = 8
-no_tc_run.enr_min = 0.01
-no_tc_run.enr_max = 0.04
-
-# Update paths for each run
-tc_run.iter_path = os.getcwd() + '/thermal_expansion/k_iter'
-no_tc_run.iter_path = os.getcwd() + '/no_thermal_expansion/k_iter'
+te_run.queue = 'fill'
+te_run.ompcores = 8
+te_run.enr_min = 0.01
+te_run.enr_max = 0.04
 
 # Find critical enrichment for both cases
-tc_run.iterate_rho(thermal_expansion = True)
-no_tc_run.iterate_rho(thermal_expansion = False)
+te_run.iterate_rho(thermal_expansion = True)
 
 # Save iteration data
-tc_run.save_iters()
-no_tc_run.save_iters()
+te_run.save_iters()
 
 # Update run parameters for rep runs
-tc_run.e0 = tc_run.conv_enr
-tc_run.rep_e = 0.07
-tc_run.queue = 'xeon'
-tc_run.ompcores = 64
-tc_run.rep_upper = 1e-9
-tc_run.rep_lower = 1e-10
-tc_run.rep_rate = 7e-9
+te_run.e0 = te_run.conv_enr
+te_run.rep_e = 0.07
+te_run.queue = 'xeon'
+te_run.ompcores = 64
+te_run.rep_upper = 1e-9
+te_run.rep_lower = 1e-10
+te_run.rep_rate = 7e-9
 
-no_tc_run.e0 = no_tc_run.conv_enr
-no_tc_run.rep_e = 0.07
-no_tc_run.queue = 'xeon'
-no_tc_run.ompcores = 64
-tc_run.rep_upper = 1e-9
-tc_run.rep_lower = 1e-10
-tc_run.rep_rate = 7e-9
-
-# Update paths for rep runs
-tc_run.rep_path = os.getcwd() + '/thermal_expansion/rep'
-no_tc_run.rep_path = os.getcwd() + '/no_thermal_expansion/rep'
+no_te_run.e0 = te_run.conv_enr
 
 # Run rep rates
-tc_run.get_rep_rate(thermal_expansion=True)
-no_tc_run.get_rep_rate(thermal_expansion=False)
+te_run.get_rep_rate(thermal_expansion=True)
+
+# Set no thermal expansion run to have same rep rate as other run
+no_te_run.rep_rate = te_run.rep_rate
 
 # Update number of histories for feedbacks
-tc_run.histories = 100000
-no_tc_run.histories = 100000
+te_run.histories = 100000
+no_te_run.histories = 100000
 
 # Update paths for feedback runs
-tc_run.feed_path = os.getcwd() + '/thermal_expansion/feedback'
-no_tc_run.feed_path = os.getcwd() + '/no_thermal_expansion/feedback'
+te_run.feed_path = os.getewd() + '/feedback_thermal_expansion'
+no_te_run.feed_path = os.getewd() + '/feedback_no_thermal_expansion'
 
 # Run feedback calculations
-tc_run.run_feedbacks(feedback='fs.dopp', thermal_expansion=True, recalc=True)
-tc_run.run_feedbacks(feedback='gr.dopp', thermal_expansion=True, recalc=True)
+te_run.run_feedbacks(feedback='fs.dopp', thermal_expansion=True, recalc=True)
+te_run.run_feedbacks(feedback='gr.dopp', thermal_expansion=True, recalc=True)
 
-no_tc_run.run_feedbacks(feedback='fs.dopp', thermal_expansion=False, recalc=True)
-no_tc_run.run_feedbacks(feedback='gr.dopp', thermal_expansion=False, recalc=True)
+# sleep to let other runs finish
+time .sleep(50000)
+
+no_te_run.run_feedbacks(feedback='fs.dopp', thermal_expansion=False, recalc=True)
+no_te_run.run_feedbacks(feedback='gr.dopp', thermal_expansion=False, recalc=True)
 
 
