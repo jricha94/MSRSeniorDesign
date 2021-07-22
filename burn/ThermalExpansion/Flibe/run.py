48d0f85a0d686cd0df14c0aacfcbb563ce9aadfe
Jarod Richardson <jricha94@vols.utk.edu>
2021-07-21 23:36:01 -0400

    restart run

--- burn/ThermalExpansion/Flibe/run.py
+++ burn/ThermalExpansion/Flibe/run.py
@@ -18,9 +18,7 @@ te_run.e0 = te_run.conv_enr
 te_run.rep_e = 0.07
 te_run.queue = 'xeon'
 te_run.ompcores = 64
-te_run.rep_upper = 1e-7
-te_run.rep_lower = 1e-9
-te_run.rep_rate = 1e-8
+
 
 no_te_run.e0 = te_run.conv_enr
 no_te_run.rep_e = 0.07
@@ -28,25 +26,25 @@ no_te_run.queue = 'xeon'
 no_te_run.ompcores = 64
 
 # Run rep rates
-te_run.get_rep_rate(thermal_expansion=True)
+rep_rate = 8.312499999999999e-09
 
 # Set no thermal expansion run to have same rep rate as other run
-no_te_run.rep_rate = te_run.rep_rate
-
+te_run.rep_rate = rep_rate
+no_te_run.rep_rate = rep_rate
 # Update number of histories for feedbacks
 te_run.histories = 100000
 no_te_run.histories = 100000
 
 # Update paths for feedback runs
-te_run.feed_path = os.getewd() + '/feedback_thermal_expansion'
-no_te_run.feed_path = os.getewd() + '/feedback_no_thermal_expansion'
+te_run.feed_path = os.getcwd() + '/feedback_thermal_expansion'
+no_te_run.feed_path = os.getcwd() + '/feedback_no_thermal_expansion'
 
 # Run feedback calculations
 te_run.run_feedbacks(feedback='fs.dopp', thermal_expansion=True, recalc=True)
 te_run.run_feedbacks(feedback='gr.dopp', thermal_expansion=True, recalc=True)
 
 # sleep to let other runs finish
-time.sleep(50000)
+time.sleep(108000)
 
 no_te_run.run_feedbacks(feedback='fs.dopp', thermal_expansion=False, recalc=True)
 no_te_run.run_feedbacks(feedback='gr.dopp', thermal_expansion=False, recalc=True)
